e76979decddf16717d9cf82f2c78949d3ce82b65
lightchainaidev <lightchainaidev@proton.me>
2026-08-09 23:18:48 +0200

    Update token-chart.tsx

--- apps/web/components/charts/token-chart.tsx
+++ apps/web/components/charts/token-chart.tsx
@@ -3,8 +3,6 @@
 import dynamic from "next/dynamic";
 import { useParams } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import Script from "next/script";
-import { useState } from "react";
 
 import tokenQuery from "@/queries/tokenQuery";
 import { Card } from "@lcai/ui/components/card";
@@ -16,19 +14,13 @@ const TVChartContainer = dynamic(() => import("@/components/TVChartContainer").t
 });
 
 export function TokenChart() {
-  const [isScriptReady, setIsScriptReady] = useState(false);
   const { address } = useParams<{ address: string }>();
   const { data: token } = useSuspenseQuery(tokenQuery(address));
 
   return (
     <Card className="overflow-hidden border-border/60 p-2">
-      <Script
-        src="/trading-view/datafeeds/udf/dist/bundle.js"
-        strategy="lazyOnload"
-        onReady={() => setIsScriptReady(true)}
-      />
       <div className="h-125 w-full">
-        {isScriptReady && address ? (
+        {address ? (
           <TVChartContainer symbol={token.symbol} address={address} />
         ) : (
           <Skeleton className="h-full w-full" />
